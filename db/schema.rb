cb6cbc2e4f2186c4bbb16556b132a41586f57aec
ryosukeohi <95561370+ryosukeohi@users.noreply.github.com>
2022-01-17 13:41:01 +0000

    [Fix]NotNull制約追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_17_093230) do
+ActiveRecord::Schema.define(version: 2022_01_17_133317) do
 
   create_table "addresses", force: :cascade do |t|
-    t.integer "customer_id"
-    t.string "name"
-    t.string "postal_code"
-    t.string "address"
+    t.integer "customer_id", null: false
+    t.string "name", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -49,11 +49,11 @@ ActiveRecord::Schema.define(version: 2022_01_17_093230) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "last_name"
-    t.string "first_name"
-    t.string "postal_code"
-    t.string "address"
-    t.string "telephone_number"
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "telephone_number", null: false
     t.boolean "is_active", default: true
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
