b5424cbdd1982ccc9227fca0ebb6b03b02588e36
ryosukeohi <95561370+ryosukeohi@users.noreply.github.com>
2022-01-17 15:34:48 +0000

    [Add]ジャンル一覧・新規登録画面

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 namespace :admin do
 root to: 'homes#top'
+resources :genres, only: [:index, :create, :edit, :update]
 end
 end
