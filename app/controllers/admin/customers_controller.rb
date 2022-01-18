268b0a406153d44a94885dde1ecc64cd53641007
ryosukeohi <95561370+ryosukeohi@users.noreply.github.com>
2022-01-18 08:37:00 +0000

    [Add]会員一覧画面

---
+++ app/controllers/admin/customers_controller.rb
@@ -0,0 +1,16 @@
+class Admin::CustomersController < ApplicationController
+
+  def index
+    @customers = Customer.page(params[:page]).reverse_order
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ namespace :admin do
 root to: 'homes#top'
 resources :genres, only: [:index, :create, :edit, :update]
 resources :items, except: [:destroy]
+resources :customers, only: [:index, :show, :edit, :update,]
 end
 end
