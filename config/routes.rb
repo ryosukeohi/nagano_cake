fe610b7e5ed01d42433feb92c423b3c7e5cc595d
ryosukeohi <95561370+ryosukeohi@users.noreply.github.com>
2022-01-29 13:52:32 +0000

    [Add]ヘッダー

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :customers, controllers: {
+  devise_for :admins, controllers: {
   sessions:      'admins/sessions',
   passwords:     'admins/passwords',
   registrations: 'admins/registrations'
 }
-  devise_for :admins, controllers: {
+
+  devise_for :customers, controllers: {
   sessions:      'customers/sessions',
   passwords:     'customers/passwords',
   registrations: 'customers/registrations'
@@ -26,13 +27,10 @@ resources :addresses, except: [:new, :show]
 delete '/cart_items/clear' => 'cart_items#clear'
 
 namespace :admin do
-root to: 'homes#top'
+get '/homes/top' => 'homes#top'
 resources :genres, only: [:index, :create, :edit, :update]
 resources :items, except: [:destroy]
 resources :customers, only: [:index, :show, :edit, :update,]
-get '/sign_in', to: 'sessions#new'
-post '/sign_in', to: 'sessions#create'
-delete '/sign_out', to: 'sessions#destroy'
 end
 
 end
