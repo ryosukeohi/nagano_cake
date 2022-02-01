f38a1a6c51559d8871c87f83e33481c1e1ae4a0b
ryosukeohi <95561370+ryosukeohi@users.noreply.github.com>
2022-02-01 15:24:13 +0000

    [Add]注文情報確認画面

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -21,11 +21,11 @@ class OrdersController < ApplicationController
   def confirm
     @order = Order.new(order_params)
     render :new if @order.invalid?
+    @address = Address.find(params[:order][:address_id])
     @order.postal_code = current_customer.postal_code
     @order.address = current_customer.address
     @order.name = current_customer.first_name + current_customer.last_name
-
-
+    @cart_items = current_customer.cart_items
   end
 
 
