fe8e3e1efa0962ba65db1ea43196c46990fa3c78
ryosukeohi <95561370+ryosukeohi@users.noreply.github.com>
2022-02-05 12:01:33 +0000

    [Fix]管理者側商品一覧

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ItemsController < ApplicationController
   before_action :authenticate_admin!
   def index
-    @items = Item.page(params[:page]).reverse_order
+    @items = Item.order("id ASC").page(params[:page]).per(5)
   end
 
   def new
@@ -10,8 +10,11 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save
-    redirect_to admin_items_path
+    if  @item.save
+      redirect_to admin_items_path
+    else
+      render :new
+    end
   end
 
   def show
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,4 +11,9 @@ class Item < ApplicationRecord
   def with_tax_price
     (price * 1.1).floor
   end
+
+  validates :name, presence: true
+  validates :image_id, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
 end
