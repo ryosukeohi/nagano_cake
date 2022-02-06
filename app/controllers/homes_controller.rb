6fb3820067409b1bdec3e6f417673af2184dc1a0
ryosukeohi <95561370+ryosukeohi@users.noreply.github.com>
2022-02-06 15:40:58 +0000

    [Fix]商品一覧画面

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,6 @@ class HomesController < ApplicationController
 
   def top
     @items = Item.all
-    p @items
   end
 
   def about
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
 
   def index
      @items = Item.page(params[:page]).reverse_order
+     @count = Item.count
   end
 
   def show
