c7e55cda5b0169b53eee7be7668fee8accaa00bc
ryosukeohi <95561370+ryosukeohi@users.noreply.github.com>
2022-02-09 04:52:38 +0000

    [Fix]名前の表示修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -42,7 +42,7 @@ before_action :authenticate_customer!
     if params[:order][:select_address] == "0"
        @order.postal_code = current_customer.postal_code
        @order.address = current_customer.address
-       @order.name = current_customer.first_name + current_customer.last_name
+       @order.name = current_customer.last_name + current_customer.first_name
     elsif params[:order][:select_address] == "1"
        @address = Address.find(params[:order][:address_id])
        @order.postal_code = @address.postal_code
